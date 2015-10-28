f87ece836ef37db306bceb4973a7c1597afa9284
khungryapple <ang_jason@naver.com>
2015-10-28 14:57:34 -0700

    deleted unecessary files and made changed in launch function

--- Ian_Fly_Wheel.h
+++ Ian_Fly_Wheel.h
@@ -1,3 +1,7 @@
+//Variables
+bool FlyWheelOn = false;
+bool HalfWheelOn = false;
+
 //Setting motors to spin in same rate(speed)
 void setFlyWheelMotor(int speed)
 {
@@ -9,13 +13,29 @@ void setFlyWheelMotor(int speed)
 
 }
 
-void launchFullFlyWheel(bool fullFlyWheelOn, bool fullFlyWheelOff) //launches the ball with the full speed
+void fullFlyWheelIsTrue(bool fullFlyWheelOn, bool fullFlyWheelOff) //launches the ball with the full speed
 {
 	if(fullFlyWheelOn == true) //port 6U-Off and 6D-On
 	{
-		setFlyWheelMotor(127);
+		FlyWheelOn = true;
 	}
 	else if (fullFlyWheelOff == true)
+	{
+		FlyWheelOn = false;
+	}
+	else
+	{
+		FlyWheelOn = false;
+	}
+}
+
+void launchFullFlyWheel()
+{
+		if(FlyWheelOn == true) //port 6U-Off and 6D-On
+	{
+		setFlyWheelMotor(127);
+	}
+	else if (FlyWheelOff == false)
 	{
 		setFlyWheelMotor(0);
 	}
@@ -27,11 +47,27 @@ void launchFullFlyWheel(bool fullFlyWheelOn, bool fullFlyWheelOff) //launches th
 
 void launchHalfFlyWheel(bool halfFlyWheelOn, bool halfFlyWheelOff) //launches the ball with the half of the speed
 {
-	if(halfFlyWheelOn == true) //port 5U-Off and 5D-On
+	if(HalfWheelOn == true)
+	{
+		HalfWheelOn = true);
+	}
+	else if (HalfWheelOff == true)
+	{
+		HalfWheelOn = false;
+	}
+	else
+	{
+	 HalfWheelOn = false;
+	}
+}
+
+void launchFullFlyWheel()
+{
+		if(HalfWheelOn == true) //port 5U-Off and 5D-On
 	{
 		setFlyWheelMotor(64);
 	}
-	else if (halfFlyWheelOff == true)
+	else if (HalfWheelOff == false)
 	{
 		setFlyWheelMotor(0);
 	}
