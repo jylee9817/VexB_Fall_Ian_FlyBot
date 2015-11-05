7d1dd31c4d61a7069f05ffc3fa7e58839f118c90
khungryapple <ang_jason@naver.com>
2015-11-05 14:27:32 -0800

    created new fly wheel code

---
+++ NewCode/IanFlyBot2.h
@@ -0,0 +1,91 @@
+//Fly Wheel that turns on and off with four button
+bool fullWheelOn = false;
+bool halfWheelOn = false;
+
+//set boolean as true
+void setFullFlyWheel(bool on)
+{
+	if(on == true)
+	{
+		fullWheelOn = true;
+		halfWheelOn = false;
+	}
+	else
+	{
+		return;
+	}
+}
+
+void setHalfFlyWheel(bool on)
+{
+	if(on == true)
+	{
+		halfWheelOn = true;
+		fullWheelOn = false;
+	}
+	else
+	{
+		return;
+	}
+}
+
+//set boolean as false
+void setFullFlyWheelOff(bool off)
+{
+	if(off == true)
+	{
+		fullWheelOn = false;
+	}
+	else
+	{
+		return;
+	}
+}
+
+void setHalfFlyWheel(bool off)
+{
+	if(off == true)
+	{
+		halfWheelOn = false;
+	}
+	else
+	{
+		return;
+	}
+}
+
+void fullFlyWheel()
+{
+	if(fullWheelOn == true)
+	{
+		motor[leftTopLauncher] = 127;
+		motor[leftBottomLauncher] = 127;
+		motor[rightTopLauncher] = 127;
+		motor[rightBottomLauncher] = 127;
+	}
+	else
+	{
+		motor[leftTopLauncher] = 0;
+		motor[leftBottomLauncher] = 0;
+		motor[rightTopLauncher] = 0;
+		motor[rightBottomLauncher] = 0;
+	}
+}
+
+void halfFlyWheel()
+{
+	if(halfWheelOn == true)
+	{
+		motor[leftTopLauncher] = 95;
+		motor[leftBottomLauncher] = 95;
+		motor[rightTopLauncher] = 95;
+		motor[rightBottomLauncher] = 95;
+	}
+	else
+	{
+		motor[leftTopLauncher] = 0;
+		motor[leftBottomLauncher] = 0;
+		motor[rightTopLauncher] = 0;
+		motor[rightBottomLauncher] = 0;
+	}
+}
--- NewCode/IanFlyWheel.h
+++ NewCode/IanFlyWheel.h
@@ -1,1 +1,37 @@
 //Fly Wheel
+
+void fullFlyWheel(bool on)
+{
+	if(on == true)
+	{
+		motor[leftTopLauncher] = 127;
+		motor[leftBottomLauncher] = 127;
+		motor[rightTopLauncher] = 127;
+		motor[rightBottomLauncher] = 127;
+	}
+	else
+	{
+		motor[leftTopLauncher] = 0;
+		motor[leftBottomLauncher] = 0;
+		motor[rightTopLauncher] = 0;
+		motor[rightBottomLauncher] = 0;
+	}
+}
+
+void halfFlyWheel(bool on)
+{
+	if(on == true)
+	{
+		motor[leftTopLauncher] = 95;
+		motor[leftBottomLauncher] = 95;
+		motor[rightTopLauncher] = 95;
+		motor[rightBottomLauncher] = 95;
+	}
+	else
+	{
+		motor[leftTopLauncher] = 0;
+		motor[leftBottomLauncher] = 0;
+		motor[rightTopLauncher] = 0;
+		motor[rightBottomLauncher] = 0;
+	}
+}
