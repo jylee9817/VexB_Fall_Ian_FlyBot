2aa824559c14dc700ed2c359d86851dd9919021a
khungryapple <ang_jason@naver.com>
2015-11-09 14:34:21 -0800

    modifying the motor value because motor is going to slow

--- NewCode/IanFlyWheel.h
+++ NewCode/IanFlyWheel.h
@@ -4,10 +4,10 @@ void fullFlyWheel(bool on)
 {
 	if(on == true)
 	{
-		motor[leftTopLauncher] = 127;
-		motor[leftBottomLauncher] = 127;
-		motor[rightTopLauncher] = 127;
-		motor[rightBottomLauncher] = 127;
+		motor[leftTopLauncher] = 120;
+		motor[leftBottomLauncher] = 120;
+		motor[rightTopLauncher] = 120;
+		motor[rightBottomLauncher] = 120;
 	}
 	else
 	{
@@ -22,10 +22,10 @@ void halfFlyWheel(bool on)
 {
 	if(on == true)
 	{
-		motor[leftTopLauncher] = 95;
-		motor[leftBottomLauncher] = 95;
-		motor[rightTopLauncher] = 95;
-		motor[rightBottomLauncher] = 95;
+		motor[leftTopLauncher] = 90;
+		motor[leftBottomLauncher] = 90;
+		motor[rightTopLauncher] = 90;
+		motor[rightBottomLauncher] = 90;
 	}
 	else
 	{
